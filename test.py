c4adaf32f7034d9e1e4d4f499ad9198a5b9075c7
Xiaotian LIU <sheldon.liuxiaotian@gmail.com>
2019-03-17 01:06:07 +1100

    remove unnessary comment

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from ObjDectector import ObjDector
-<<<<<<< HEAD
 from centroidtracker import CentroidTracker
 from object_tracking import track_object
 import cv2 as cv
@@ -9,7 +8,6 @@ detectedObjects = []
 ct = CentroidTracker()
 
 
-<<<<<<< HEAD
 # while True:
 x = ObjDector.ObjDector()
 
@@ -23,22 +21,15 @@ detectedObjects = track_object(ct, items, [image[0], image[1]])
 
     # x.drawBox(items, confidences, boxes, image)
     # cv.imwrite("./test/testresult.jpg", image.astype(np.uint8))
-=======
-=======
 import cv2 as cv
 import numpy as np
 
 
 
->>>>>>> 1b5a30006579c93b409f27a7404112ede5b7c0b2
 def test(image, tmp):
     
     items, cata, confidences, boxes = tmp.detect(image)
-    tmp.drawBox(items, cata, confidences, boxes,image)    
-<<<<<<< HEAD
->>>>>>> f3eeb19ab100a49a98512561fb90937182b89401
-=======
->>>>>>> 1b5a30006579c93b409f27a7404112ede5b7c0b2
+    tmp.drawBox(items, cata, confidences, boxes,image)
 
 
 
--- zheTest.py
+++ zheTest.py
@@ -13,12 +13,8 @@ from faceRec import faceRecgnizer
 # cv.imwrite("./test/testresult.jpg", image.astype(np.uint8))
 
 x = faceRecgnizer.FaceRecgnizer()
-<<<<<<< HEAD
 x.predict("./initialFaces/s3/WIN_20190317_00_02_50_Pro.jpg")
-=======
 face, confidence = x.predict("./initialFaces/s3/WIN_20190317_00_02_50_Pro.jpg")
-
->>>>>>> 1b5a30006579c93b409f27a7404112ede5b7c0b2
 
 
 
