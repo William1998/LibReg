d66d20ea3cb24d3402684319bd3e23f1eca9ed30
mirekzhang <wszy54@gmail.com>
2019-03-16 17:33:51 +1100

    updated - change cv2 to cv

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import cv2
+import cv2 as cv
 import numpy
 import socket
 import struct
@@ -10,14 +10,14 @@ PORT=10000
 server=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #socket对象
 server.connect((HOST,PORT))
 print('now starting to send frames...')
-capture= cv2.VideoCapture(0)
+capture= cv.VideoCapture(0)
 try:
     while True:
         print('TEST')
         success,frame=capture.read()
         while not success and frame is None:
             success,frame=capture.read()  #获取视频帧
-        result,imgencode=cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,50])  #编码
+        result,imgencode=cv.imencode('.jpg',frame,[cv.IMWRITE_JPEG_QUALITY,50])  #编码
         #server.sendall(struct.pack('i',imgencode.shape[0])) #发送编码后的字节长度，这个值不是固定的
         server.sendall(imgencode) #发送视频帧数据
         print('have sent one frame')
