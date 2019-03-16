fc7875c14e2a145573c65e2d7afe178637c9cda9
iron008@bitbucket.com <xwei0012@student.monash.edu>
2019-03-16 17:22:22 +1100

    add a place holder

--- image_receive.py
+++ image_receive.py
@@ -14,25 +14,14 @@ def AcceptImage(HOST = '192.168.43.79', PORT = 10000):
     print("Now waitting for the frame")
     
     while True:
-        
-        #print("TEST!")
-        
-        #data, address = server.recvfrom(buffersize)
-        #if len(data) == 1 and data[0] == 1:
-        #    server.close()
-        #    cv2.destroyAllWindows()
-        #    exit()
-        #if len(data) != 4:
-        #    length = 0
-        #else:
-        #    length = struct.unpack('i', data)[0] #Length value
         data, address = server.recvfrom(buffersize) # receive image
-        #if length != len(data):
-        #    continue
         data = numpy.array(bytearray(data))
         imagedecode = cv2.imdecode(data, 1)
         print("Received one frame")
         cv2.imshow('frames', imagedecode)
+        
+        # Waitting for frame process function
+        
         if cv2.waitKey(1) == 27:
             break
         
