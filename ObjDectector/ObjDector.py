c0b5cf2095587e96b0caf794652038713707c2b0
nobodyczcz <jianshen858@gmail.com>
2019-03-16 22:28:36 +1100

    "add obj detection"

--- ObjDectector/ObjDector.py
+++ ObjDectector/ObjDector.py
@@ -2,9 +2,9 @@ import cv2 as cv
 import numpy as np
 
 class ObjDector():
-    def __init__(self):
-        self.confThreshold = 0.4  # Confidence threshold
-        self.nmsThreshold = 0.4  # Non-maximum suppression threshold
+    def __init__(self, modelPath):
+        self.confThreshold = 0.2  # Confidence threshold
+        self.nmsThreshold = 0.3  # Non-maximum suppression threshold
         self.inpWidth = 288  # Width of network's input image
         self.inpHeight = 288  # Height of network's input image
 
@@ -16,69 +16,13 @@ class ObjDector():
 
         # Give the configuration and weight files for the model and load the network using them.
         modelConfiguration = "./pre-trained-model/yolov3.cfg";
-        modelWeights = "./pre-trained-model/yolov3.weights";
+        modelWeights = modelPath;
 
         self.net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
         self.net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
         self.net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
         print("Initialized")
-
-    def drawPred(self,classId, conf, left, top, right, bottom, frame):
-        # Draw a bounding box.
-        cv.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
-
-        label = '%.2f' % conf
-
-        # Get the label for the class name and its confidence
-        if self.classes:
-            assert (classId < len(self.classes))
-            label = '%s:%s' % (self.classes[classId], label)
-
-        # Display the label at the top of the bounding box
-        labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-        top = max(top, labelSize[1])
-        cv.rectangle(frame, (left, top - round(1.5 * labelSize[1])), (left + round(1.5 * labelSize[0]), top + baseLine),
-                     (255, 255, 255), cv.FILLED)
-        cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 1)
-
-    # Remove the bounding boxes with low confidence using non-maxima suppression
-    def postprocess(self,frame, outs):
-        frameHeight = frame.shape[0]
-        frameWidth = frame.shape[1]
-
-        # Scan through all the bounding boxes output from the network and keep only the
-        # ones with high confidence scores. Assign the box's class label as the class with the highest score.
-        classIds = []
-        confidences = []
-        boxes = []
-        for out in outs:
-            for detection in out:
-                scores = detection[5:]
-                classId = np.argmax(scores)
-                confidence = scores[classId]
-                if confidence > self.confThreshold:
-                    center_x = int(detection[0] * frameWidth)
-                    center_y = int(detection[1] * frameHeight)
-                    width = int(detection[2] * frameWidth)
-                    height = int(detection[3] * frameHeight)
-                    left = int(center_x - width / 2)
-                    top = int(center_y - height / 2)
-                    classIds.append(classId)
-                    confidences.append(float(confidence))
-                    boxes.append([left, top, width, height])
-
-        # Perform non maximum suppression to eliminate redundant overlapping boxes with
-        # lower confidences.
-        indices = cv.dnn.NMSBoxes(boxes, confidences, self.confThreshold, self.nmsThreshold)
-        for i in indices:
-            i = i[0]
-            box = boxes[i]
-            left = box[0]
-            top = box[1]
-            width = box[2]
-            height = box[3]
-            self.drawPred(classIds[i], confidences[i], left, top, left + width, top + height,frame)
 
 
     def getOutputsNames(self,net):
@@ -176,7 +120,6 @@ class ObjDector():
         blob = cv.dnn.blobFromImage(frame, 1 / 255, (self.inpWidth, self.inpHeight), [0, 0, 0], 1, crop=False)
         self.net.setInput(blob)
         outs = self.net.forward(self.getOutputsNames(self.net))
-
 
         return self.getCoord(frame,outs)
 
