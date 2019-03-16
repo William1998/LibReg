fb88d45ed2340745db76f1a54c4e9acaac3eaf14
Xiaotian LIU <sheldon.liuxiaotian@gmail.com>
2019-03-16 22:59:01 +1100

    update test.py

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
 from ObjDectector import ObjDector
+from centroidtracker import CentroidTracker
+from object_tracking import track_object
 import cv2 as cv
 import numpy as np
 
+detectedObjects = []
+ct = CentroidTracker()
+
+
+# while True:
 x = ObjDector.ObjDector()
 
-cap = cv.VideoCapture("./test/test1.jpg")
+cap = cv.VideoCapture("./test/test1.jpeg")
 retval, image = cap.read()
 items, confidences, boxes = x.detect(image)
+print(boxes[0])
 
-x.drawBox(items, confidences, boxes,image)
-cv.imwrite("./test/testresult.jpg", image.astype(np.uint8))
+detectedObjects = track_object(ct, items, [image[0], image[1]])
+# print(detectedObjects)
+
+    # x.drawBox(items, confidences, boxes, image)
+    # cv.imwrite("./test/testresult.jpg", image.astype(np.uint8))
 
 
 
